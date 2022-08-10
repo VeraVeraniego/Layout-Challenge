b60fb8ed116baf0677c36ffb3f26eae43a614315
Brayan Vera Vera <veraniego@pop-os.localdomain>
2022-08-10 17:23:50 -0500

    styles: correct styles

--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -8,17 +8,22 @@ const SideBarContainer = styled.section`
   width: 16.5vw;
 `;
 const Gradient = styled.div`
+  margin-top: 36px;
+  margin-left: 32px;
   width: 74px;
   height: 21px;
   background: linear-gradient(270deg, #689ef8 0%, #ff6860 56.25%, #ff9f00 100%);
   border-radius: 5px;
 `;
 const TabsContainer = styled.ul`
+  margin-top: 51px;
   color: ${palette.darkgray};
   display: flex;
   list-style: none;
   flex-direction: column;
   gap: 33px;
+  user-select: none;
+  cursor: pointer;
 `;
 export const LeftSideBar = () => {
   return (

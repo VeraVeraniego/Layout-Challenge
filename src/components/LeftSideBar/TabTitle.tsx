3f040a7194af1caa4144243e0d175559b430a3a4
Brayan Vera <brayanvera@ravn.co>
2022-08-10 17:42:51 -0500

    style: add style to TabTitle component

--- src/components/LeftSideBar/TabTitle.tsx
+++ src/components/LeftSideBar/TabTitle.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import palette from "../../theme";
 const ListItem = styled.li`
   width: 100%;
   display: flex;
+  align-items: center;
   & > span {
-    align-items: center;
     margin-left: 19px;
     vertical-align: middle;
     text-align: center;
   }
 `;
 const Rectangle = styled.div`
-  display: inline-block;
+  margin-left: 25px;
   width: 15px;
   height: 15px;
   border: 1px solid ${palette.darkgray};
+`;
+const Highlight = styled("div")<{ active: boolean }>`
+  height: 44px;
+  width: 7px;
+  background-color: ${(props) => (props.active ? "#fff" : "inherit")};
+  border-bottom-right-radius: 7px;
+  border-top-right-radius: 7px;
 `;
 export const TabTitle = ({ title }: { title: string }) => {
   return (

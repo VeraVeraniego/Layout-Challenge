8ca5c4e1f6b3a076a7c55eb2cc2a4f958d685b06
Brayan Vera <brayanvera@ravn.co>
2022-08-11 18:13:23 -0500

    feat: add only user section to aside

--- src/components/AsideRight/AsideRight.tsx
+++ src/components/AsideRight/AsideRight.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import palette from "../../theme";
+import { UserSection } from "./UserSection";
 const AsideContainer = styled.section`
   width: 24.5vw;
   background-color: ${palette.bglightgray};
 `;
 export const AsideRight = () => {
-  return <AsideContainer>AsideRight</AsideContainer>;
+  return (
+    <AsideContainer>
+      <UserSection />
+    </AsideContainer>
+  );
 };

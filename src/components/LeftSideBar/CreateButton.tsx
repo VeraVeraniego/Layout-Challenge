85a677ebde5607523c1dbf06bc8a9251e9e6f9ba
Brayan Vera <brayanvera@ravn.co>
2022-08-11 16:57:13 -0500

    feat: move createbutton as an independent component

---
+++ src/components/LeftSideBar/CreateButton.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "../../theme";
+import { SquareBorder } from "../shapes/SquareBorder";
+
+export const CreateButton = () => {
+  const optionsToCreate = [
+    "Upload files",
+    "Upload folder",
+    "New folder",
+    "More",
+  ];
+  return (
+    <>
+      <DialogOptionsContainer>
+        {optionsToCreate.map((ele, index) => {
+          return (
+            <Option key={index}>
+              <SquareBorder color="bglightgray" />
+              <span>{ele}</span>
+            </Option>
+          );
+        })}
+      </DialogOptionsContainer>
+      <Button>
+        <span>Create New</span>
+        <div>+</div>
+      </Button>
+    </>
+  );
+};
+const Option = styled.div`
+  color: ${palette.white};
+  display: flex;
+  flex-direction: row;
+  margin-left: 15%;
+  margin-top: 5px;
+  & > span {
+    margin-left: 7px;
+  }
+`;
+const DialogOptionsContainer = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  right: 0;
+  left: 0;
+  bottom: 12vh;
+  /* margin-top: auto; */
+  position: absolute;
+  padding-top: 14px;
+  width: 12vw;
+  height: 110px;
+  background-color: ${palette.dialog};
+  /* position: absolute; */
+  border-radius: 9px;
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -35px;
+    border: 25px solid transparent;
+    border-top-color: ${palette.dialog};
+    width: 0px;
+    border-radius: 15px;
+  }
+`;
+const Button = styled.button`
+  position: absolute;
+  margin-top: 90vh;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  border: none;
+  width: 9.9vw;
+  height: 30px;
+  background-color: ${palette.orange};
+  color: ${palette.white};
+  border-radius: 7px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & > span {
+    font-size: 10px;
+    margin: auto;
+  }
+  & > div {
+    width: 21px;
+    height: 21px;
+    vertical-align: middle;
+    line-height: 21px;
+    background-color: ${palette.lightorange};
+    border-radius: 4px;
+    margin-left: auto;
+    margin-right: 4px;
+  }
+`;

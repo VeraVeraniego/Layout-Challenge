2127d81173c4c5b6af4e4576e8898f298b8b2b0f
Brayan Vera <BrayanVera@ravn.co>
2022-08-12 13:29:55 -0500

    refactor: clean a unused import

--- src/components/AsideRight/UserSection.tsx
+++ src/components/AsideRight/UserSection.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import palette from "../../theme";
 import { Square } from "../shapes/Square";
 
 export const UserSection = () => {

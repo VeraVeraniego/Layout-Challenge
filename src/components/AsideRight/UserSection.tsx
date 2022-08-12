449f0405405d94aa68e351633e2f78f26e8c55f0
Brayan Vera <brayanvera@ravn.co>
2022-08-12 18:13:43 -0500

    feat: finished user section on top of aside

--- src/components/AsideRight/UserSection.tsx
+++ src/components/AsideRight/UserSection.tsx
@@ -5,16 +5,44 @@ import { Square } from "../shapes/Square";
 export const UserSection = () => {
   return (
     <UserInfoContainer>
-      <Square color="inactive" width={15} />
-      <Square color="inactive" width={15} />
-      {/* <UserData */}
+      <Group1>
+        <Square color="inactive" width={15} />
+        <Square color="inactive" width={15} />
+      </Group1>
+      <Group2>
+        <span>Name</span>
+        <ProfilePic src="https://rickandmortyapi.com/api/character/avatar/5.jpeg" />
+      </Group2>
     </UserInfoContainer>
   );
 };
-const UserInfoContainer = styled.section`
+const ProfilePic = styled.img`
+  width: 3vw;
+  height: 3vw;
+  border-radius: 50%;
+`;
+const Group1 = styled.div`
+  margin-left: 13.4%;
   display: flex;
+  gap: 13px;
+  /* background-color: #000; */
+`;
+const Group2 = styled(Group1)`
+  margin-left: 0;
+  margin-right: 13.4%;
   align-items: center;
-  justify-content: space-around;
+  & > {
+    font-size: 12px;
+    line-height: 14px;
+  }
+`;
+const UserInfoContainer = styled.section`
+  padding-top: 25px;
+  padding-bottom: 25px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 // const Test = styled.div`
 //   width: 15px;

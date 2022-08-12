98fd4430d3b563e0ecc2bb36f6393277cdceac46
Brayan Vera <BrayanVera@ravn.co>
2022-08-12 13:22:30 -0500

    refactor: move styled component below the functional component

--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -1,37 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import palette, { GlobalStyle } from "../../theme";
+import { defaultTheme, GlobalStyle } from "../../theme";
 import { CreateButton } from "./CreateButton";
 import { TabTitle } from "./TabTitle";
-
-const SideBarContainer = styled.section`
-  background-color: ${palette.darkblue};
-  width: 16.5vw;
-  position: relative;
-`;
-const Gradient = styled.div`
-  margin-top: 36px;
-  margin-left: 32px;
-  width: 74px;
-  height: 21px;
-  background: linear-gradient(270deg, #689ef8 0%, #ff6860 56.25%, #ff9f00 100%);
-  border-radius: 5px;
-`;
-const TabsContainer = styled.ul`
-  margin-top: 51px;
-  color: ${palette.darkgray};
-  display: flex;
-  list-style: none;
-  flex-direction: column;
-  gap: 4px;
-  user-select: none;
-  cursor: pointer;
-
-  & > hr {
-    color: #464c61;
-    margin-top: 15px;
-  }
-`;
 
 export const LeftSideBar = () => {
   const [activeTab, setActiveTab] = useState<number>(0);
@@ -71,3 +42,32 @@ export const LeftSideBar = () => {
     </SideBarContainer>
   );
 };
+
+const SideBarContainer = styled.section`
+  background-color: ${defaultTheme.palette.darkblue};
+  width: 16.5vw;
+  position: relative;
+`;
+const Gradient = styled.div`
+  margin-top: 36px;
+  margin-left: 32px;
+  width: 74px;
+  height: 21px;
+  background: linear-gradient(270deg, #689ef8 0%, #ff6860 56.25%, #ff9f00 100%);
+  border-radius: 5px;
+`;
+const TabsContainer = styled.ul`
+  margin-top: 51px;
+  color: ${defaultTheme.palette.darkgray};
+  display: flex;
+  list-style: none;
+  flex-direction: column;
+  gap: 4px;
+  user-select: none;
+  cursor: pointer;
+
+  & > hr {
+    color: #464c61;
+    margin-top: 15px;
+  }
+`;

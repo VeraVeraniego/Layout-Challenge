aa9c8261b20c5382efeca90f63a9d39d7ebf5e6b
Brayan Vera <brayanvera@ravn.co>
2022-08-13 12:00:15 -0500

    refactor: move styled components to bottom and styling layout components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,34 @@ import { AsideRight } from "./components/AsideRight/AsideRight";
 import { LeftSideBar } from "./components/LeftSideBar/LeftSideBar";
 import { MainContent } from "./components/MainContent/MainContent";
 import { defaultTheme, GlobalStyle } from "./theme";
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <PageContainer>
+        <LeftSideBarFlexed></LeftSideBarFlexed>
+        <MainContentFlexed></MainContentFlexed>
+        <AsideRightFlexed></AsideRightFlexed>
+      </PageContainer>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+const LeftSideBarFlexed = styled(LeftSideBar)`
+  width: 16.5vw;
+
+  /* flex-basis: 200px; */
+`;
+const MainContentFlexed = styled(MainContent)`
+  width: 55.8vw;
+`;
+const AsideRightFlexed = styled(AsideRight)`
+  /* flex-basis: 252px; */
+  width: 24.5vw;
+`;
+
 const PageContainer = styled.div`
   background-color: #fff;
   width: 100vw;
@@ -11,17 +39,3 @@ const PageContainer = styled.div`
   display: flex;
   gap: 1.6vw;
 `;
-function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <PageContainer>
-        <GlobalStyle />
-        <LeftSideBar></LeftSideBar>
-        <MainContent></MainContent>
-        <AsideRight></AsideRight>
-      </PageContainer>
-    </ThemeProvider>
-  );
-}
-
-export default App;

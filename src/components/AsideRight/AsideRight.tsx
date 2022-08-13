74ffd7e36f738ef0be9894ad0ad6520e1e714cc6
Brayan Vera <brayanvera@ravn.co>
2022-08-13 12:41:26 -0500

    feat: finished aside on the right

--- src/components/AsideRight/AsideRight.tsx
+++ src/components/AsideRight/AsideRight.tsx
@@ -14,16 +14,58 @@ export const AsideRight = ({ className }: { className?: string }) => {
       <HR />
       <AsideBodyContainer>
         <StorageChartAndInfo />
-        <Square
+        <UpgradeContainer
           color={defaultTheme.palette.white}
-          width={194}
           height={202}
           borderRadius="18px"
-        ></Square>
+        >
+          <Gradient width={119} height={73} borderRadius="11px" />
+          <TextUpgrade>
+            Buy more space now!
+            <br />
+            <span>Upgrade to cloud premium</span>
+          </TextUpgrade>
+          <Button>Upgrade Account!</Button>
+        </UpgradeContainer>
       </AsideBodyContainer>
     </AsideContainer>
   );
 };
+const Button = styled.button`
+  font-size: 10px;
+  line-height: 12px;
+  background-color: ${defaultTheme.palette.orange};
+  color: ${defaultTheme.palette.white};
+  border: none;
+  width: auto;
+  height: 30px;
+  border-radius: 7px;
+  padding: 10px 18px;
+  display: inline-flex;
+  align-items: center;
+`;
+const TextUpgrade = styled.h3`
+  font-size: 12px;
+  line-height: 14px;
+
+  text-align: center;
+  & > span {
+    font-size: 8px;
+    line-height: 9px;
+    color: ${defaultTheme.palette.darkgray};
+  }
+`;
+const Gradient = styled(Square)`
+  background: linear-gradient(270deg, #689ef8 0%, #ff6860 56.25%, #ff9f00 100%);
+`;
+const UpgradeContainer = styled(Square)`
+  margin-top: 30px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+`;
 const AsideBodyContainer = styled.div`
   margin: 1.5vw 3vw;
 `;

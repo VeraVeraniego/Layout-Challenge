374ba82538eda5266781cb5667de3e00e8a3ba34
Brayan Vera <BrayanVera@ravn.co>
2022-08-13 19:00:28 -0500

    feat: add search bar to layout

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 import { defaultTheme } from "../../theme";
+import { Square } from "../shapes/Square";
+export const MainContent = ({ className }: { className?: string }) => {
+  return (
+    <MainSection className={className}>
+      <SearchBar color={defaultTheme.palette.white}>
+        <Square borderColor="gray" />
+        <Input placeholder="Search" />
+      </SearchBar>
+    </MainSection>
+  );
+};
+const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  color: ${defaultTheme.palette.darkblue};
+  font-size: 10px;
+  line-height: 12px;
+  border: none;
+  margin-left: 7px;
+`;
+const SearchBar = styled(Square)`
+  width: 100%;
+  height: 29px;
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  padding: 7px 12px;
+`;
 const MainSection = styled.section`
+  padding: 25px 18px;
   background-color: ${defaultTheme.palette.bglightgray};
 `;
-export const MainContent = ({ className }: { className?: string }) => {
-  return <MainSection className={className}>MainSection</MainSection>;
-};
